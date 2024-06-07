288b55fb5ec771064e9c4ec9f0f3ccfd63a1013f
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2024-06-07 19:02:46 -0400

    adjust gradient and spacing

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen bg-navy-950 flex-col items-center justify-between p-6 md:p-24 relative">
       <div className="max-w-screen-lg">
-        <div className="relative flex flex-col w-full py-10 md:py-16 px-5 md:px-10 max-h-[400px] gap-3 md:gap-10 mt-24">
+        <div className="relative flex flex-col w-full py-10 md:py-16 px-5 md:px-10 max-h-[400px] gap-5 md:gap-10 mt-24">
           <p className="text-sm md:text-lg text-amber-100">
             Hi, nice to meet you!
           </p>
@@ -48,13 +48,13 @@ export default function Home() {
             <h1 className="flex items-center gap-2 md:gap-3 text-3xl md:text-6xl text-slate-400 font-bold ">
               that are
               <motion.span
-                className="text-left bg-gradient-to-br text-transparent py-1 md:py-2 bg-clip-text from-navy-400 via-navy-300 to-navy-400"
+                className="text-left bg-gradient-to-br text-transparent py-1 md:py-2 bg-clip-text from-navy-400 via-navy-500 to-navy-400 from-40% via-70% to-100%"
                 style={{ translateX: promptX, opacity }}
               >
-                {descriptors[currDescriptor]}
+                {descriptors[currDescriptor]}.
               </motion.span>
             </h1>
-            <Balancer className="text-sm md:text-lg font-medium text-slate-500 mt-4 md:mt-12 max-w-[40em]">
+            <Balancer className="text-sm md:text-lg font-medium text-slate-500 mt-4 md:mt-6 max-w-[40em]">
               I'm a founder, software engineer, product person, and{" "}
               {
                 <SimpleLink href={"https://www.instagram.com/buzzlifegames/"}>
