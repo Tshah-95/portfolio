9c148b1aa24108e3e51bac992234f864f5f07ee4
Tejas Shah <61719799+Tshah-95@users.noreply.github.com>
2024-06-07 19:33:27 -0400

    fix package error and add initial animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import Link from "next/link";
-import { animate, motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  animate,
+  backIn,
+  easeOut,
+  motion,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
 import { useEffect, useState } from "react";
 import Balancer from "react-wrap-balancer";
 
@@ -23,7 +30,7 @@ export default function Home() {
       });
     };
 
-    const interval = setInterval(() => animateText(), 2500);
+    const interval = setInterval(() => animateText(), 3500);
 
     return () => clearInterval(interval);
   }, []);
@@ -35,17 +42,34 @@ export default function Home() {
     <main className="flex min-h-screen bg-navy-950 flex-col items-center justify-between p-6 md:p-24 relative">
       <div className="max-w-screen-lg">
         <div className="relative flex flex-col w-full py-10 md:py-16 px-5 md:px-10 max-h-[400px] gap-5 md:gap-10 mt-24">
-          <p className="text-sm md:text-lg text-amber-100">
+          <motion.p
+            className="text-sm md:text-lg text-amber-100"
+            initial={initialState}
+            custom={0}
+            animate="slide"
+            variants={variants}
+          >
             Hi, nice to meet you!
-          </p>
-          <h1 className="text-3xl md:text-6xl text-white font-bold">
+          </motion.p>
+          <motion.h1
+            className="text-3xl md:text-6xl text-white font-bold"
+            initial={initialState}
+            custom={1}
+            animate="slide"
+            variants={variants}
+          >
             My name is Tejas.
-          </h1>
-          <div>
+          </motion.h1>
+          <motion.div
+            initial={initialState}
+            custom={2}
+            animate="slide"
+            variants={variants}
+          >
             <h1 className="flex items-center text-3xl md:text-6xl text-slate-400 font-bold">
               I build products
             </h1>
-            <h1 className="flex items-center gap-2 md:gap-3 text-3xl md:text-6xl text-slate-400 font-bold ">
+            <h1 className="flex items-center gap-2 md:gap-3 text-3xl md:text-6xl text-slate-400 font-bold">
               that are
               <motion.span
                 className="text-left bg-gradient-to-br text-transparent py-1 md:py-2 bg-clip-text from-navy-400 via-navy-500 to-navy-400 from-40% via-70% to-100%"
@@ -54,13 +78,18 @@ export default function Home() {
                 {descriptors[currDescriptor]}.
               </motion.span>
             </h1>
-            <Balancer className="text-sm md:text-lg font-medium text-slate-500 mt-4 md:mt-6 max-w-[40em]">
+          </motion.div>
+          <motion.div
+            initial={initialState}
+            custom={3}
+            animate="slide"
+            variants={variants}
+          >
+            <Balancer className="text-sm md:text-lg font-medium text-slate-500 max-w-[40em]">
               I&apos;m a founder, software engineer, product person, and{" "}
-              {
-                <SimpleLink href={"https://www.instagram.com/buzzlifegames/"}>
-                  content creator
-                </SimpleLink>
-              }
+              <SimpleLink href={"https://www.instagram.com/buzzlifegames/"}>
+                content creator
+              </SimpleLink>
               . I try my best to deliver experiences that make people&apos;s
               lives easier. Recently, that&apos;s{" "}
               <SimpleLink href={"https://www.perytonai.com"}>
@@ -68,12 +97,28 @@ export default function Home() {
               </SimpleLink>
               .
             </Balancer>
-          </div>
+          </motion.div>
         </div>
       </div>
     </main>
   );
 }
+
+const initialState = { y: -50, opacity: 0 };
+
+const variants = {
+  slide: (custom: number) => ({
+    opacity: 1,
+    y: 0,
+    x: 0,
+    initial: {},
+    transition: {
+      type: "spring",
+      duration: 2,
+      delay: custom * 0.35 + 0.5,
+    },
+  }),
+};
 
 const SimpleLink = ({
   href,
